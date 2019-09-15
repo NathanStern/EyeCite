3f5d3f84e981846e23d324ecb51cc56abd99faee
Nathan <nstern@wearelcc.ca>
2019-09-15 00:23:47 -0400

    added login functionality to Content.js

--- src/pages/content/Content.js
+++ src/pages/content/Content.js
@@ -2,16 +2,49 @@ import React from 'react';
 import './Content.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function Index() {
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../../firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+    googleProvider: new firebase.auth.GoogleAuthProvider()
+};
+
+
+
+function Index(props) {
+  const {
+    user,
+    signOut,
+    signInWithGoogle,
+  } = props;
+
   return (
     <div className="App">
       <h1  align="left">MyFridge</h1>
       <header className="App-header">
-        <Link to="/login/">Login</Link>
+        {
+          user 
+            ? <></>
+            : <p>Please sign in.</p>
+        }
+        {
+          user
+            ? <></>
+            : <button className="googleButton" onClick={signInWithGoogle}>Sign in with Google</button>
+        }
       </header>
     
     </div>
   );
 }
 
-export default Index;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+}) (Index);
--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -2,61 +2,22 @@ import React from 'react';
 import './Index.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-
-import withFirebaseAuth from 'react-with-firebase-auth'
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from '../../firebaseConfig';
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-const firebaseAppAuth = firebaseApp.auth();
-
-const providers = {
-    googleProvider: new firebase.auth.GoogleAuthProvider()
-};
-
-function homeRedirect() {
-  window.location.pathname = "/"
-}
-
-
-
-function renderItems(props) {
-  const user = props.user;
-  let itemComponents = null;
-
-
+function signInAndRedirect() {
+  window.location.pathname = "/content/"
 }
 
 
 function Index(props) {
-  const {
-    user,
-    signOut,
-    signInWithGoogle,
-  } = props;
+
   return (
     <div className="App">
       <h1  align="left">MyFridge</h1>
       <header className="App-header">
-      {
-          user 
-            ? <p>Hello, {user.displayName}</p>
-            : <p>Please sign in.</p>
-        }
-        {
-          user
-            ? <button className="googleButton" onClick={signOut}>Sign out</button>
-            : <button className="googleButton" onClick={signInWithGoogle}>Sign in with Google</button>
-        }
+      
       </header>
     
     </div>
   );
 }
 
-export default  withFirebaseAuth({
-  providers,
-  firebaseAppAuth
-}) (Index);
+export default (Index);
