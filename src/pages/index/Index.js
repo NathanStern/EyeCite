880089cc3efdc0b97ece059b0d3e6ac8952b0259
Nathan <nstern@wearelcc.ca>
2019-09-14 23:00:53 -0400

    kjsdf

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Index.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+function loginPageRedirect() {
+  window.location.pathname = "/login/";
+}
+
+
+
 function Index() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Link to="/login/">Login</Link>
-      </header>
+      
     </div>
   );
 }
