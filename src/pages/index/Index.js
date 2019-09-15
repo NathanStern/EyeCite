e9b678829b6e9089f4bb80c20fd5a3ffb81ea440
Nathan <nstern@wearelcc.ca>
2019-09-14 23:47:59 -0400

    removed the login page

--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -2,22 +2,61 @@ import React from 'react';
 import './Index.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function loginPageRedirect() {
-  window.location.pathname = "/login/";
+
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from '../../firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+    googleProvider: new firebase.auth.GoogleAuthProvider()
+};
+
+function homeRedirect() {
+  window.location.pathname = "/"
 }
 
 
 
-function Index() {
+function renderItems(props) {
+  const user = props.user;
+  let itemComponents = null;
+
+
+}
+
+
+function Index(props) {
+  const {
+    user,
+    signOut,
+    signInWithGoogle,
+  } = props;
   return (
     <div className="App">
       <h1  align="left">MyFridge</h1>
       <header className="App-header">
-        <Link to="/login/">Login</Link>
+      {
+          user 
+            ? <p>Hello, {user.displayName}</p>
+            : <p>Please sign in.</p>
+        }
+        {
+          user
+            ? <button className="googleButton" onClick={signOut}>Sign out</button>
+            : <button className="googleButton" onClick={signInWithGoogle}>Sign in with Google</button>
+        }
       </header>
     
     </div>
   );
 }
 
-export default Index;
+export default  withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+}) (Index);
