70b1bc9ab2b4661e3536a1289229a7080bf8cb39
Nathan <nstern@wearelcc.ca>
2019-09-15 00:00:46 -0400

    added content route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Index from "./pages/index/Index";
+import Content from "./pages/content/Content";
 
 
 function App() {
     return (
       <Router>
           <Route path="/" exact component={Index} />
+          <Route path="/content/" component={Content} />
       </Router>
     );
   }
