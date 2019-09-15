3d1c3d7054bc4782273b55fd2460d063ab640a89
Claire Wallace <walla148@purdue.edu>
2019-09-15 00:02:03 -0400

    changed some stuff - claire

--- src/pages/content/Content.js
+++ src/pages/content/Content.js
@@ -6,10 +6,22 @@ function Index() {
   return (
     <div className="App">
       <h1  align="left">MyFridge</h1>
-      <header className="App-header">
-        <Link to="/login/">Login</Link>
-      </header>
-    
+      <div id= "table">
+          <form method="post" action="myfridge.php">
+              <table>
+                  <tr>
+                       <th>Food Item</th>
+                       <th>Expiration Date</th>
+                       <th>Action</th>
+                  </tr>
+                  <tr>
+                      <td><input type="text" name="food"></input></td>
+                      <td><input type="date" date="expirationDate"></input></td>
+                      <td><input type="delete" name="Delete"></input></td>
+                  </tr>
+              </table>
+          </form>
+      </div>
     </div>
   );
 }
--- src/pages/index/Index.js
+++ src/pages/index/Index.js
@@ -7,7 +7,7 @@ function Index() {
     <div className="App">
       <h1  align="left">MyFridge</h1>
       <header className="App-header">
-        <Link to="/login/">Login</Link>
+        <Link to="/login/"><h3 align="left">Login</h3></Link>
       </header>
     
     </div>
